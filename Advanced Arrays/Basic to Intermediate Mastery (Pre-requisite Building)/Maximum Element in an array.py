1e78f0360f05d0e410736e3ad5bf56937bc3900e
Ankita Ghosh <91538328+Phoenixcoder-6@users.noreply.github.com>
2025-06-29 01:48:49 +0530

    Update Maximum Element in an array.py

--- Advanced Arrays/Basic to Intermediate Mastery (Pre-requisite Building)/Maximum Element in an array.py
+++ Advanced Arrays/Basic to Intermediate Mastery (Pre-requisite Building)/Maximum Element in an array.py
@@ -1,6 +1,6 @@
-def max_arr(arr):
-    return max(arr)
+def maxmin_arr(arr):
+    return max(arr),min(arr)
 
 arr=[2,3,4,5,6]
-res= max_arr(arr)
-print(res)
+res1,res2= maxmin_arr(arr)
+print(res1,res2)
